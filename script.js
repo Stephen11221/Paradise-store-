32979ac0952eda56f2cdd5d012fb85caac722ca2
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2025-03-12 23:35:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// Mobile Navbar Toggle
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', function() {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
+
+    // Hide loader after 3 seconds
+        setTimeout(() => {
+            document.getElementById('loadingScreen').style.display = 'none';
+        }, 3000);
+// Image Slider Logic
+
+let currentIndex = 0;
+const slides = document.querySelectorAll("#slider > div");
+const totalSlides = slides.length;
+
+function updateSlide() {
+    document.getElementById("slider").style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+// Auto-slide every 3 seconds
+setInterval(() => {
+    nextSlide();
+}, 3000);
+
+function prevSlide() {
+    currentIndex = (currentIndex === 0) ? totalSlides - 1 : currentIndex - 1;
+    updateSlide();
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex === totalSlides - 1) ? 0 : currentIndex + 1;
+    updateSlide();
+}
+
+        // Auto-slide for Customer Reviews
+        let index = 0;
+        function slideReviews() {
+            const slider = document.getElementById("reviewSlider");
+            index = (index + 1) % 2; // Change number based on total reviews
+            slider.style.transform = `translateX(-${index * 100}%)`;
+        }
+        setInterval(slideReviews, 3000);
+        // brand animation 
+        document.addEventListener("DOMContentLoaded", function () {
+            const text = "EXCLUSIVE PERFUME BRANDS";
+            const animatedText = document.getElementById("animatedBrandText");
+            let index = 0;
+        
+            function typeText() {
+              if (index < text.length) {
+                let span = document.createElement("span");
+                span.textContent = text[index];
+                span.style.opacity = "0";
+                span.style.animation = "fadeInLetter 0.5s ease-out forwards";
+                span.style.animationDelay = `${index * 0.1}s`; // Delay each letter
+                animatedText.appendChild(span);
+                index++;
+                setTimeout(typeText, 50); // Adjust speed here
+              }
+            }
+        
+            typeText();
+          });
+ 
+          // brand  animal
+          
+         
